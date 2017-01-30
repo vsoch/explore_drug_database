c4283596acbc1719cc842bf43ee87ae84d7060a4
jomuller <jomuller@jom.link>
2017-01-30 07:14:51 +0100

    Follow the rules of reproductible analysis for this project

---
+++ run_all.R
@@ -0,0 +1,9 @@
+# Run all
+source("rscripts/make_reports.R")
+
+source("functions/make_reports.R")
+default_open_file <- TRUE
+
+report("01-Download_raw_data.Rmd")
+report("02-Import.Rmd")
+report("03-Explore_bdpm.Rmd")
